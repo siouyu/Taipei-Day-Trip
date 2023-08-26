d0ad796e4f82d4345246409cf811041b58aef255
Siouyu Hsu <siouyu@SiouyudeMacBook-Air.local>
2023-08-26 16:19:21 +0800

    update host=0.0.0.0

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def home():
     return render_template("home.html")
     
 if __name__ == "__main__":
-    app.run(port = 8080, debug = True)
+    app.run(host="0.0.0.0", port = 8080, debug = True)
     app.secret_key = "secret"
