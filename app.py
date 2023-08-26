bac907e0e407fff6dd416af9cbfb683ca4b4725d
Siouyu Hsu <siouyu@SiouyudeMacBook-Air.local>
2023-08-26 16:12:34 +0800

    check port 8080

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import *
 
 app = Flask(__name__)
-app.secret_key = "secret"
+
+print("gala gala")
 
 @app.route("/") 
 def home():
+    print("嘎拉嘎拉")
     # if session.get("username"):
     #     return redirect("/member")
     # else:
     return render_template("home.html")
     
 if __name__ == "__main__":
-    app.run(port = 3000, debug = True)
+    app.run(port = 8080, debug = True)
     app.secret_key = "secret"
