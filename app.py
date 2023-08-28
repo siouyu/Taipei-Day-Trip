113721a5a19b6471b3fbb4093f5ad6d6e80aa0ca
Siouyu Hsu <siouyu@SiouyudeMacBook-Air.local>
2023-08-28 16:29:48 +0800

    taipei-day-trip

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import *
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-print("gala gala")
+app.config["JSON_AS_ASCII"]=False
+app.config["TEMPLATES_AUTO_RELOAD"]=True
 
-@app.route("/") 
-def home():
-    print("閃電！")
-    # if session.get("username"):
-    #     return redirect("/member")
-    # else:
-    return render_template("home.html")
-    
+# Pages
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/attraction/<id>")
+def attraction(id):
+	return render_template("attraction.html")
+
+@app.route("/booking")
+def booking():
+	return render_template("booking.html")
+
+@app.route("/thankyou")
+def thankyou():
+	return render_template("thankyou.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 8080, debug = True)
     app.secret_key = "secret"
